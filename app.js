be462445441f89cf762556e4390c7255cd5444a2
isikli <isiklia@gmail.com>
2020-03-01 13:58:57 +1100

    log port id

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
-
+console.log(`Example app listening on port ${port}!`)
 
 app.use(cors());
 app.use(bodyParser.json());
